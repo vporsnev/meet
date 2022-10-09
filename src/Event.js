4f339977af0403f8ff4d051cf2ed2f1d535398a2
vporsnev <v.porshnev@gmail.com>
2022-10-09 16:46:09 +0200

    4.3
    Testing for first three features

---
+++ src/Event.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+class Event extends Component {
+    toggleEventDetails = () => {
+        this.setState({ show: !this.state.show });
+      };
+
+      state = { show: false };
+
+  render() {
+    const { event } = this.props;
+    return (
+        <div className="event">
+            <h1 className="event-title">{event.summary}</h1>
+            <p className="event-info">
+            {event.start.dateTime} {event.start.timeZone} {event.location}
+          </p>
+          {!this.state.show ? (
+            <button
+              className="showDetails-button"
+              onClick={this.toggleEventDetails}
+            >
+              Show Details
+            </button>
+          ) : (
+            <button
+              className="hideDetails-button"
+              onClick={this.toggleEventDetails}
+            >
+              Hide Details
+            </button>
+          )}
+          {this.state.show && (
+          <div className="expandedEvent">
+            <p className="event-description">{event.description}</p>
+            <h3>Ends: </h3><p className="event-end">{event.end.dateTime}</p>
+            <h3>Creator: </h3><p>{event.creator.email}</p>
+            <a
+                href={event.htmlLink}
+                target="_blank"
+                className="event-link"
+              >
+                Event on Google Calendar
+              </a>
+          </div>
+          )}
+        </div>
+        );
+    }
+  }
+  
+export default Event;
---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../Event';
+import { mockData } from '../mock-data';
+
+describe('<Event /> component', () => {
+    let event, EventWrapper;
+    beforeAll(() => {
+      event = mockData[0];
+
+      EventWrapper = shallow(<Event event={event} />);
+  });
+
+  test('render event title', () => {
+    expect(EventWrapper.find('.event-title')).toHaveLength(1);
+  });
+
+  test('render event title correctly', () => {
+    expect(EventWrapper.find('.event-title').text()).toBe(
+      event.summary
+    );
+  });
+
+  test('render info in event item', () => {
+    expect(EventWrapper.find('.event-info')).toHaveLength(1);
+  });
+
+  test('render event info correctly', () => {
+    expect(EventWrapper.find('.event-info').text()).toContain(
+      event.start.dateTime
+    );
+    expect(EventWrapper.find('.event-info').text()).toContain(
+      event.start.timeZone
+    );
+    expect(EventWrapper.find('.event-info').text()).toContain(event.location);
+  });
+
+  test('render event collapsed by default', () => {
+    expect(EventWrapper.state('show')).toBe(false);
+  });
+
+  test('render show details button in the collapsed event', () => {
+    expect(EventWrapper.find('.showDetails-button')).toHaveLength(1);
+  });
+
+  test('render click to show event details', () => {
+    EventWrapper.setState({
+      show: false
+    });
+    EventWrapper.find('.showDetails-button').simulate('click');
+    expect(EventWrapper.state('show')).toBe(true);
+  });
+
+  test('render click to hide event details', () => {
+    EventWrapper.setState({
+      show: true
+    });
+    EventWrapper.find('.hideDetails-button').simulate('click');
+    expect(EventWrapper.state('show')).toBe(false);
+  });
+
+  test('render expanded event', () => {
+    EventWrapper.setState({
+      show: true
+    });
+    expect(EventWrapper.find('.expandedEvent').text()).toContain(
+      event.description
+    );
+    expect(EventWrapper.find('.expandedEvent').text()).toContain(
+      event.end.dateTime
+    );
+    expect(EventWrapper.find('.hideDetails-button')).toHaveLength(1);
+  });
+
+  test('render after click hide event details', () => {
+    EventWrapper.setState({
+      show: false
+    });
+
+    expect(EventWrapper.find('.expandedEvent')).toHaveLength(0);
+  });
+
+});
