b6241e14bfb3800b7239e37f5f862c26a80ff12b
vporsnev <v.porshnev@gmail.com>
2022-10-10 11:09:37 +0300

    4.4 features and styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
+import { getEvents, extractLocations } from './api';
 
 class App extends Component {
+
+  state = {
+    events: [],
+    locations: []
+  }
+
+    updateEvents = (location, eventCount) => {
+      if (location === undefined) {
+        location = this.state.seletedLocation;
+      }
+      if (eventCount === undefined) {
+        eventCount = this.state.numberOfEvents;
+      }
+      getEvents().then((events) => {
+        const locationEvents = (location === 'all') ?
+          events :
+          events.filter((event) => event.location === location);
+        this.setState({
+          events: locationEvents,
+          numberOfEvents: eventCount
+        });
+      });
+    }
+
+    componentDidMount() {
+      this.mounted = true;
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
+  
+    componentWillUnmount(){
+      this.mounted = false;
+    }
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList /> 
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <NumberOfEvents updateEvents={this.updateEvents}/>
+        <EventList events={this.state.events} />
       </div>
     );
   }
