76c8469e9827cc38fb99fded3199c894271701bc
vporsnev <v.porshnev@gmail.com>
2022-10-21 18:45:04 +0200

    4.8 PWA Service Workers, Verification for google

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@
     return locations;
   };
 
-  const checkToken = async (accessToken) => {
+  export const checkToken = async (accessToken) => {
     const result = await fetch(
       `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
     )
